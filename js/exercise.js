a702ab80098e37d7730f9058ea5b54d95b9bb267
Joshua Lam <jlam26.ca@gmail.com>
2020-11-23 04:13:57 -0800

    exercise.html frontend template setup

---
+++ js/exercise.js
@@ -0,0 +1,7 @@
+
+function deleteExercise(exercise){
+    console.log(exercise);
+}
+
+console.log("test");
+document.getElementById("manualDelete").addEventListener("click", function(){deleteExercise("BenchPress");});
