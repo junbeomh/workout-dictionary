555b53c10e5e3bb81e82f265ef4c2703b189d63d
JunBeom Han <junbeomh94@gmail.com>
2020-11-24 08:54:52 -0800

    updated index

--- js/index.js
+++ js/index.js
@@ -62,7 +62,7 @@ const excercises = [
 ]
 
 const filterSearch = (value) => {
-    $('#searchResults div').each(function () {
+    $('#searchResults .card-header').each(function () {
         let found = 'false';
         $(this).each(function () {
             if ($(this).text().toLowerCase().indexOf(value.toLowerCase()) >= 0) {
@@ -80,37 +80,75 @@ const filterSearch = (value) => {
   </div>
 </div> */}
 
-const makeExcercise = (excercise) => {
-    const cardColumns = document.getElementById("searchResults");
-    let cardContainer = document.createElement("div");
-    let card = document.createElement("div");
-    let cardBody = document.createElement("div");
-    let cardName = document.createElement("p");
-    let cardType = document.createElement("p");
-    let cardDescription = document.createElement("p");
-    cardContainer.setAttribute('class', 'col-lg-4 col-md-4 col-sm-12 col-xs-12');
-    card.setAttribute('class', 'card bg-light');
-    cardBody.setAttribute('class', 'card bg-light');
-    card.setAttribute('class', 'card-body text-center');
-    cardName.setAttribute('class', 'card-text');
-    cardType.setAttribute('class', 'card-text');
-    cardDescription.setAttribute('class', 'card-text');
-    cardName.innerHTML = excercise.name;
-    cardType.innerHTML = excercise.type;
-    cardDescription.innerHTML = excercise.description;
-    cardBody.appendChild(cardType);
-    cardBody.appendChild(cardName);
-    cardBody.appendChild(cardDescription);
-    card.appendChild(cardBody);
-    cardColumns.appendChild(card);
+const makeExcercise = (excercise, index) => {
+    let cardColumns = document.getElementById("searchResults");
+    let workoutContainer = document.createElement("div");
+    let workoutHeader = document.createElement("div");
+    let workoutBody = document.createElement("div");
+    let workoutTitle = document.createElement("a");
+    let workoutDescript = document.createElement("div");
+    let workoutType = document.createElement("p");
+    let favouriteBtn = document.createElement("button");
+
+    favouriteBtn.setAttribute('type', 'button');
+    favouriteBtn.setAttribute('class', 'btn btn-secondary btn-sm');
+    favouriteBtn.innerHTML = "add";
+
+    workoutContainer.setAttribute('class', 'card');
+    workoutHeader.setAttribute('class', 'card-header');
+    workoutTitle.setAttribute('id', 'cardTitle');
+    workoutTitle.setAttribute('data-toggle', 'collapse');
+    workoutTitle.setAttribute('data-toggle', 'collapse');
+    workoutTitle.setAttribute('href', `#workout-descript-${index}`);
+    workoutTitle.setAttribute('aria-expanded', 'true');
+    workoutTitle.setAttribute('aria-control', `workout-descript-${index}`);
+
+    workoutTitle.innerHTML = excercise.type.toUpperCase() + ": " + excercise.name;
+    workoutHeader.appendChild(workoutTitle);
+    workoutHeader.appendChild(favouriteBtn);
+
+    workoutBody.setAttribute('class', 'collapse');
+    workoutBody.setAttribute('id', `workout-descript-${index}`);
+    workoutDescript.setAttribute('class', 'card-block');
+
+    workoutDescript.innerHTML = excercise.description;
+    workoutBody.appendChild(workoutDescript);
+
+    workoutContainer.appendChild(workoutHeader);
+    workoutContainer.appendChild(workoutBody);
+
+    cardColumns.appendChild(workoutContainer);
+
+    // const cardColumns = document.getElementById("searchResults");
+    // let cardContainer = document.createElement("div");
+    // let card = document.createElement("div");
+    // let cardBody = document.createElement("div");
+    // let cardName = document.createElement("p");
+    // let cardType = document.createElement("p");
+    // let cardDescription = document.createElement("p");
+    // cardContainer.setAttribute('class', 'col-lg-4 col-md-4 col-sm-12 col-xs-12');
+    // card.setAttribute('class', 'card bg-light');
+    // cardBody.setAttribute('class', 'card bg-light');
+    // card.setAttribute('class', 'card-body text-center');
+    // cardName.setAttribute('class', 'card-text');
+    // cardType.setAttribute('class', 'card-text');
+    // cardDescription.setAttribute('class', 'card-text');
+    // cardName.innerHTML = excercise.name;
+    // cardType.innerHTML = excercise.type;
+    // cardDescription.innerHTML = excercise.description;
+    // cardBody.appendChild(cardType);
+    // cardBody.appendChild(cardName);
+    // cardBody.appendChild(cardDescription);
+    // card.appendChild(cardBody);
+    // cardColumns.appendChild(myWorkOuts);
 }
 
 const getAllExcercises = () => {
     // make api call
 
     // process api response 
-    excercises.forEach((excercise) => {
-        makeExcercise(excercise);
+    excercises.forEach((excercise, index) => {
+        makeExcercise(excercise, index);
     })
 }
 
