007e141db48202629aa5da4cf96b4ce47606930e
Joshua Lam <jlam26.ca@gmail.com>
2020-11-23 16:25:29 -0800

    deleted duplicate scripts

--- js/exercise.js
+++ js/exercise.js
@@ -1,7 +1,13 @@
-
-function deleteExercise(exercise){
-    console.log(exercise);
+ // Todo - delete from db
+ function deleteExercise(exercise) {
+    let card = document.getElementById(exercise);
+    card.remove();
 }
 
-console.log("test");
-document.getElementById("manualDelete").addEventListener("click", function(){deleteExercise("BenchPress");});
+//Todo - save into db
+function saveExercise(exercise){
+    console.log("Todo - save into db")
+}
+
+// Will be manually added to element when created dynamically when page loaded
+document.getElementById("manualDelete").addEventListener("click", function () { deleteExercise("BenchPress"); });
