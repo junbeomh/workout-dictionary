fb3d1975d8659dfb452d3d7782f1e3b8641463b3
Joshua Lam <jlam26.ca@gmail.com>
2020-11-29 08:35:16 -0800

    dynamically create elements with sudoData

--- js/profile.js
+++ js/profile.js
@@ -3,11 +3,26 @@ var ACCESS_TOKEN = localStorage.getItem("ACCESS_TOKEN");
 console.log(ID_TOKEN);
 console.log(ACCESS_TOKEN);
 
+let sudoData = [
+    "Chest Day",
+    "Back Day",
+    "Leg Day"
+]
+
+function createWorkout(workout) {
+    let workoutBtn = document.createElement("button");
+    workoutBtn.setAttribute("id", workout);
+    workoutBtn.setAttribute("class", "dropdown-item border mb-2");
+    workoutBtn.innerHTML = workout
+    workoutBtn.addEventListener("click", function () { editWorkout(workout);});
+    document.getElementById("workouts").appendChild(workoutBtn);
+}
+
 function editWorkout(workout) {
     localStorage.setItem("name", workout);
     window.location.href = "workoutEditor.html";
 }
 
-document.getElementById("Chest Day").addEventListener("click", function () { editWorkout("Chest Day"); });
-document.getElementById("Back Day").addEventListener("click", function () { editWorkout("Back Day"); });
-document.getElementById("Leg Day").addEventListener("click", function () { editWorkout("Leg Day"); });
+sudoData.forEach(element => {
+    createWorkout(element);
+});
--- js/workoutEditor.js
+++ js/workoutEditor.js
@@ -12,6 +12,34 @@ function removeExercise(exercise){
     card.remove();
 }
 
-document.getElementById("hardcodeBench").addEventListener("click", function () { removeExercise("Bench"); });
-document.getElementById("hardcodeInclineBench").addEventListener("click", function () { removeExercise("InclineBench"); });
+let sudoData = ["Bench", "Incline", "test"]
+
+function createExercise(exercise) {
+    let container = document.createElement("div");
+    let titleDiv = document.createElement("div");
+    let title = document.createElement("p");
+    let buttonDiv = document.createElement("div");
+    let button = document.createElement("button");
+
+    container.setAttribute("id", exercise);
+    container.setAttribute("class", "row mb-2");
+    titleDiv.setAttribute("class", "col-2");
+    title.innerHTML = exercise;
+    titleDiv.appendChild(title);
+    container.appendChild(titleDiv);
+    buttonDiv.setAttribute("class","col-10");
+    button.setAttribute("id", exercise + "Btn");
+    button.setAttribute("class", "btn btn-secondary")
+    button.innerHTML = "X";
+    button.addEventListener("click", function () { removeExercise(exercise); });
+    buttonDiv.appendChild(button);
+    container.appendChild(buttonDiv);
+
+    document.getElementById("workout").appendChild(container);
+}
+
+sudoData.forEach(element => {
+    createExercise(element);
+});
+
 document.getElementById("delete").addEventListener("click", deleteWorkout);
