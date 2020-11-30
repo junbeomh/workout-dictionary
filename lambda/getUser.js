d9e739d558cbc64aaaa847fa649ffa26ac6a7461
JunBeom Han <junbeomh94@gmail.com>
2020-11-29 22:42:47 -0800

    added admin rule

--- lambda/getUser.js
+++ lambda/getUser.js
@@ -9,14 +9,14 @@ const tableName = "workout-dictionary-users"
 
 
 exports.handler = (event, context, callback) => {
-    var res ={
+    var res = {
         "statusCode": 200,
         "headers": {
-            "Content-Type": "*/*",
             "Access-Control-Allow-Origin": "*",
-            }
-        };
-        
+            'Content-Type': 'application/json',
+        }
+    };
+
     console.log("uid: " + event.uid);
 
     var params = {
@@ -26,12 +26,20 @@ exports.handler = (event, context, callback) => {
         }
     };
 
+    let message = {
+        'message': 'Execution started successfully!'
+    }
 
-    docClient.get(params, function(err, data){
+
+    docClient.get(params, function (err, data) {
         if (err) {
             callback(err, null);
-        }else{
-            callback(null, data)
+        } else {
+            callback(null, {
+                statusCode: 200,
+                body: JSON.stringify(data),
+                headers: { 'Content-Type': 'application/json' }
+            });
         }
     })
 };
