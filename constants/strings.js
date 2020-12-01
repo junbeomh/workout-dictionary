b76e50dc6b01fbb8791e313e3d443280dc42b8cc
JunBeom Han <junbeomh94@gmail.com>
2020-11-30 18:25:05 -0800

    update

--- constants/strings.js
+++ constants/strings.js
@@ -4,7 +4,6 @@ var API_ADD_TO_FAVOURITES = 'https://d8rmqw1449.execute-api.us-west-2.amazonaws.
 
 var API_GET_USER = 'https://d8rmqw1449.execute-api.us-west-2.amazonaws.com/prod/getuser?uid=';
 
-
 var API_REMOVE_FAVOURITES = 'https://d8rmqw1449.execute-api.us-west-2.amazonaws.com/prod/deletefavourites';
 
 
