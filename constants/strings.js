f232a78929453b7dda33d305e39fafaeef3b61ab
JunBeom Han <junbeomh94@gmail.com>
2020-11-30 20:54:12 -0800

    last push

--- constants/strings.js
+++ constants/strings.js
@@ -4,6 +4,7 @@ var API_ADD_TO_FAVOURITES = 'https://d8rmqw1449.execute-api.us-west-2.amazonaws.
 
 var API_GET_USER = 'https://d8rmqw1449.execute-api.us-west-2.amazonaws.com/prod/getuser?uid=';
 
+
 var API_REMOVE_FAVOURITES = 'https://d8rmqw1449.execute-api.us-west-2.amazonaws.com/prod/deletefavourites';
 
 
@@ -11,3 +12,4 @@ var API_GET_EXCERCISES = 'https://d8rmqw1449.execute-api.us-west-2.amazonaws.com
 
 
 var API_ADD_TO_DICTIONARY = 'https://d8rmqw1449.execute-api.us-west-2.amazonaws.com/prod/createexercise';
+
